552dcc65968009f3c316ad844a65392af57882e2
Pedro Colle <pedro.h.b.colle@gmail.com>
2022-09-05 23:38:07 -0300

    mudanças no fim da noite q não levaram a nada, esse negocio ta pronto

--- src/io/disp.rs
+++ src/io/disp.rs
@@ -1,4 +1,5 @@
-//use ansi_term::{Style, Color::{Black, White}};
+//use ansi_term::Color::{ White, RGB};
+//White.bold().on(RGB(250, 148, 5)).paint(
 
 use crate::io::parser::{Player};
 use core::time;
@@ -13,12 +14,14 @@ pub fn display_time_elapsed(time:time::Duration){
 }
 
 pub fn head_row_player(){
-	println!("|{:>9}|{:>40}|{:>17}|{:>7.2}|{:>6}",
+	println!("\n{sep}{:>9}{sep}{:>40}{sep}{:>17}{sep}{:>7.2}{sep}{:>6}",
 		"sofifa_id",
 		"name",
 		"player_positions",
 		"rating",
-		"count");
+		"count",
+		sep = "|"
+	);
 }
 
 pub fn display_row_player(p : &Player){
@@ -31,12 +34,14 @@ pub fn display_row_player(p : &Player){
 }
 
 pub fn head_row_user_review(){
-	println!("|{:>9}|{:>40}|{:>14}|{:>6}|{:>7}",
+	println!("\n{sep}{:>9}{sep}{:>40}{sep}{:>14}{sep}{:>6}{sep}{:>7}",
 		"sofifa_id",
 		"name",
 		"global_rating",
 		"count",
-		"rating");
+		"rating",
+		sep = "|"
+	);
 }
 
 pub fn display_row_user_reviews(p : &Player, raiting: &f64){
